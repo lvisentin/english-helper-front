64a7429268eb407b9b35f7e398c041d34b5efb13
lvisentin <lvise.batista@gmail.com>
2023-06-20 12:48:39 -0300

    feat: adding new layouts and pages

--- components/layouts/primary/PrimaryLayout.stories.tsx
+++ components/layouts/primary/PrimaryLayout.stories.tsx
@@ -1,19 +1,19 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react';
-import BaseTemplate, { IBaseTemplate } from './BaseTemplate';
+import PrimaryLayout, { IPrimaryLayout } from './PrimaryLayout';
 
 export default {
-  title: 'templates/BaseTemplate',
-  component: BaseTemplate,
+  title: 'templates/PrimaryLayout',
+  component: PrimaryLayout,
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
   argTypes: {},
-} as ComponentMeta<typeof BaseTemplate>;
+} as ComponentMeta<typeof PrimaryLayout>;
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template: ComponentStory<typeof BaseTemplate> = (args) => (
-  <BaseTemplate {...args} />
+const Template: ComponentStory<typeof PrimaryLayout> = (args) => (
+  <PrimaryLayout {...args} />
 );
 
 export const Base = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
 
-Base.args = {} as IBaseTemplate;
+Base.args = {} as IPrimaryLayout;
---
+++ pages/index.tsx
@@ -0,0 +1,25 @@
+import PrimaryLayout from '../components/layouts/primary/PrimaryLayout';
+import SidebarLayout from '../components/layouts/sidebar/SidebarLayout';
+import styles from '../styles/Home.module.scss';
+import { NextPageWithLayout } from './page';
+
+const Home: NextPageWithLayout = () => {
+  return (
+    <section className={styles.main}>
+      <h1 className={styles.title}>
+        Welcome to <a href="https://nextjs.org">Next.js!</a>
+      </h1>
+    </section>
+  );
+};
+
+export default Home;
+
+Home.getLayout = (page) => {
+  return (
+    <PrimaryLayout>
+      <SidebarLayout />
+      {page}
+    </PrimaryLayout>
+  );
+};
