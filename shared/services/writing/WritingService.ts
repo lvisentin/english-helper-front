d7e324b5a1586307188a8bd8baec1dee3de48253
lvisentin <lvise.batista@gmail.com>
2023-06-22 11:02:01 -0300

    feat: creating user service and adding authtoken to writingservice

--- shared/services/writing/WritingService.ts
+++ shared/services/writing/WritingService.ts
@@ -1,9 +1,8 @@
 import axios from 'axios';
-
+import { userService } from '../user/UserService';
 class WritingService {
   private API_URL = process.env.API_URL;
-  private authToken =
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNjQ3ODc5MTg2ODBlY2Y4NmUwYTUwZjUxIiwiZW1haWwiOiJsdmlzZS5iYXRpc3RhQGdtYWlsLmNvbSIsImlhdCI6MTY4NzM5MDc2NiwiZXhwIjoxNjg3OTk1NTY2fQ.pNnhsKcIfvqHkNQW5-AaGS4wc7m3A8jWlrkCsnP4RZY';
+  private authToken = userService.getAuthToken();
 
   getWritings() {
     return axios.get(`${this.API_URL}/feedbacks`, {
