1a9d7593802458581725d6fe0ef1da53dacb1ee1
lvisentin <lvise.batista@gmail.com>
2023-06-22 11:38:06 -0300

    feat: installing font awesome

--- components/layouts/sidebar/SidebarLayout.tsx
+++ components/layouts/sidebar/SidebarLayout.tsx
@@ -1,4 +1,5 @@
-import Link from 'next/link';
+import { faLockOpen } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import styles from './SidebarLayout.module.scss';
 
 export interface ISidebarLayout {}
@@ -6,10 +7,25 @@ export interface ISidebarLayout {}
 const SidebarLayout: React.FC<ISidebarLayout> = () => {
   return (
     <nav className={styles.nav}>
-      <input className={`${styles.input} input`} placeholder="Search..." />
+      <div className={styles.userInfo}>
+        <img src="https://placehold.it/50x50" alt="Avatar" />
+        <p className={styles.userName}>Lucas Visentin</p>
+        <p className={styles.userEmail}>lvise.batista@gmail.com</p>
+      </div>
+
+      <div className={styles.navBody}>
+        <ul className={styles.itemsList}>
+          <li>
+            <FontAwesomeIcon icon={faLockOpen} />
+            Home
+          </li>
+        </ul>
+      </div>
+
+      {/* <input className={`${styles.input} input`} placeholder="Search..." />
       <Link href="/">Home </Link>
       <Link href="/about">About</Link>
-      <Link href="/contact">Contact</Link>
+      <Link href="/contact">Contact</Link> */}
     </nav>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,10 @@
+import { GlobalStateContext } from '@/context/GlobalState/Context';
+import { GlobalStateProvider } from '@/context/GlobalState/Provider';
+import { useContext } from 'react';
 import PrimaryLayout from '../components/layouts/primary/PrimaryLayout';
 import SidebarLayout from '../components/layouts/sidebar/SidebarLayout';
 import styles from '../styles/Home.module.scss';
 import { NextPageWithLayout } from './page';
-import { useContext } from 'react';
-import { GlobalStateContext } from '@/context/GlobalState/Context';
-import { GlobalStateProvider } from '@/context/GlobalState/Provider';
 
 const Home: NextPageWithLayout = () => {
   const { globalState, dispatch } = useContext(GlobalStateContext);
@@ -15,7 +15,10 @@ const Home: NextPageWithLayout = () => {
     <section className={styles.main}>
       <h1 className={styles.title}>current branch: develop</h1>
       <h2 className={'p-10 pl-0'}>{globalState?.user?.name}</h2>
-      <button className={'btn'} onClick={updateGlobalState}>
+      <button className={'btn btn-primary'} onClick={updateGlobalState}>
+        show state
+      </button>
+      <button className={'btn btn-secondary'} onClick={updateGlobalState}>
         show state
       </button>
     </section>
