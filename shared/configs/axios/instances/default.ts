f426c85a7b873ee372f6148cfdec0e288dbe3438
lvisentin <lvise.batista@gmail.com>
2023-07-02 09:56:01 -0300

    feat: axios default instance with resposne handlers

---
+++ shared/configs/axios/instances/default.ts
@@ -0,0 +1,41 @@
+import { userService } from '@/shared/services/user/UserService';
+import axios from 'axios';
+
+const customInstance = axios.create({
+  baseURL:
+    'http://english-helper-app-env-1.eba-ajas73zf.us-east-1.elasticbeanstalk.com/api/v1',
+  timeout: 10000,
+});
+
+const requestHandler = (request) => {
+  request.headers.Authorization = `Bearer ${userService.getAuthToken()}`;
+
+  return request;
+};
+
+const responseHandler = (response) => {
+  if (response.status === 401) {
+    console.error('UNAUTHENTICATED');
+  }
+
+  return response;
+};
+
+const errorHandler = (error) => {
+  if (error.response.status === 401) {
+    console.error('UNAUTHENTICATED');
+  }
+  return Promise.reject(error);
+};
+
+customInstance.interceptors.request.use(
+  (request) => requestHandler(request),
+  (error) => errorHandler(error)
+);
+
+customInstance.interceptors.response.use(
+  (response) => responseHandler(response),
+  (error) => errorHandler(error)
+);
+
+export default customInstance;
