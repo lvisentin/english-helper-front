7dd3dc6a0185488f077da0bf10557a812540d924
lvisentin <lvise.batista@gmail.com>
2023-07-03 18:33:28 -0300

    fix: changing primary layout and internal layout

---
+++ app/internal/layout.tsx
@@ -0,0 +1,23 @@
+import Sidebar from '@/components/Sidebar/Sidebar';
+import Head from 'next/head';
+import React from 'react';
+
+export interface IInternalLayout {
+  children: React.ReactNode;
+}
+
+const InternalLayout: React.FC<IInternalLayout> = ({ children }) => {
+  return (
+    <>
+      <Head>
+        <title>Primary Layout Example</title>
+      </Head>
+      <div className={`main flex flex-row h-full w-full`}>
+        <Sidebar />
+        <main className={'p-8 flex-grow bg-slate-100'}>{children}</main>
+      </div>
+    </>
+  );
+};
+
+export default InternalLayout;
