36df70a45a78f92013b10cdcf17732fac172fa24
lvisentin <lvise.batista@gmail.com>
2023-07-03 18:27:26 -0300

    fix: fix layout

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,8 +1,6 @@
 'use client';
 
 import TextField from '@/components/TextField/TextField';
-import LoginSplitLayout from '@/components/layouts/login/LoginSplitLayout';
-import loginImgSrc from '@/resources/a-dude.png';
 import eyeSlashSvgSrc from '@/resources/svg/eye-slash.svg';
 import eyeSvgSrc from '@/resources/svg/eye.svg';
 import lockSvgSrc from '@/resources/svg/lock.svg';
@@ -37,119 +35,105 @@ export default function LoginPage() {
   }
 
   return (
-    <LoginSplitLayout>
-      <div
-        className={
-          'prose prose-a:no-underline card bg-base-100 rounded-md w-full max-w-xl sm:p-6 sm:shadow-md sm:border-[1px] sm:border-gray-200'
-        }
+    <div
+      className={
+        'prose prose-a:no-underline card bg-base-100 rounded-md w-full max-w-xl sm:p-6 sm:shadow-md sm:border-[1px] sm:border-gray-200'
+      }
+    >
+      <h2 className={'text-center mt-0'}>Bem vindo de volta!</h2>
+      <p className={'text-center'}>
+        Novo aqui?{' '}
+        <span className={'prose-a:hover:underline'}>
+          <Link href={'/sign-up'} className={'text-primary'}>
+            {' '}
+            Crie uma conta
+          </Link>
+        </span>
+      </p>
+
+      <Formik
+        initialValues={{ email: '', password: '' }}
+        onSubmit={(values) => login(values.email, values.password)}
       >
-        <h2 className={'text-center mt-0'}>Bem vindo de volta!</h2>
-        <p className={'text-center'}>
-          Novo aqui?{' '}
-          <span className={'prose-a:hover:underline'}>
-            <Link href={'/sign-up'} className={'text-primary'}>
-              {' '}
-              Crie uma conta
-            </Link>
-          </span>
-        </p>
-
-        <Formik
-          initialValues={{ email: '', password: '' }}
-          onSubmit={(values) => login(values.email, values.password)}
-        >
-          {({ values, handleChange, handleBlur, handleSubmit }) => (
-            <>
-              <form
-                className={'grid grid-cols-10 gap-x-4 gap-y-2'}
-                onSubmit={handleSubmit}
-              >
-                <TextField
-                  placeholder={'Seu email'}
-                  label={'Email'}
-                  name="email"
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  value={values.email}
-                  leadingIcon={
+        {({ values, handleChange, handleBlur, handleSubmit }) => (
+          <>
+            <form
+              className={'grid grid-cols-10 gap-x-4 gap-y-2'}
+              onSubmit={handleSubmit}
+            >
+              <TextField
+                placeholder={'Seu email'}
+                label={'Email'}
+                name="email"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.email}
+                leadingIcon={
+                  <Image
+                    src={mailSvgSrc}
+                    alt={'icone representando uma carta'}
+                    className={`w-6 h-6 max-w-6 ${styles.svg}`}
+                  />
+                }
+                className={'col-span-10'}
+              />
+              <TextField
+                placeholder={'Sua senha'}
+                label={'Senha'}
+                name="password"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.password}
+                leadingIcon={
+                  <Image
+                    src={lockSvgSrc}
+                    alt={'icone representando um cadeado'}
+                    width={24}
+                    className={`w-6 h-6 ${styles.svg}`}
+                  />
+                }
+                trailingIcon={{
+                  type: 'toggle',
+                  initialIcon: (
                     <Image
-                      src={mailSvgSrc}
-                      alt={'icone representando uma carta'}
-                      className={`w-6 h-6 max-w-6 ${styles.svg}`}
+                      src={eyeSvgSrc}
+                      alt={'icone representando um olho humano'}
+                      className={`w-6 h-6 swap-on ${styles.svg}`}
                     />
-                  }
-                  className={'col-span-10'}
-                />
-                <TextField
-                  placeholder={'Sua senha'}
-                  label={'Senha'}
-                  name="password"
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  value={values.password}
-                  leadingIcon={
+                  ),
+                  secondIcon: (
                     <Image
-                      src={lockSvgSrc}
-                      alt={'icone representando um cadeado'}
-                      width={24}
-                      className={`w-6 h-6 ${styles.svg}`}
+                      src={eyeSlashSvgSrc}
+                      alt={'icone representando um olho humano riscado'}
+                      className={`w-6 h-6 swap-off ${styles.svg}`}
                     />
-                  }
-                  trailingIcon={{
-                    type: 'toggle',
-                    initialIcon: (
-                      <Image
-                        src={eyeSvgSrc}
-                        alt={'icone representando um olho humano'}
-                        className={`w-6 h-6 swap-on ${styles.svg}`}
-                      />
-                    ),
-                    secondIcon: (
-                      <Image
-                        src={eyeSlashSvgSrc}
-                        alt={'icone representando um olho humano riscado'}
-                        className={`w-6 h-6 swap-off ${styles.svg}`}
-                      />
-                    ),
-                  }}
-                  className={'col-span-10'}
-                />
+                  ),
+                }}
+                className={'col-span-10'}
+              />
 
-                <div className={'flex flex-col w-full col-span-10'}>
-                  <p className={'text-right my-4'}>
-                    <span
-                      className={'prose-a:no-underline prose-a:hover:underline'}
-                    >
-                      <Link href={'#'}>Esqueceu sua senha?</Link>
-                    </span>
-                  </p>
-
-                  <LoadingButton
-                    loading={loading}
-                    disabled={loading}
-                    className={`${styles.loginBtn} btn btn-primary mt-6`}
-                    type="submit"
+              <div className={'flex flex-col w-full col-span-10'}>
+                <p className={'text-right my-4'}>
+                  <span
+                    className={'prose-a:no-underline prose-a:hover:underline'}
                   >
-                    Entrar
-                  </LoadingButton>
-                </div>
-              </form>
-            </>
-          )}
-        </Formik>
-      </div>
+                    <Link href={'#'}>Esqueceu sua senha?</Link>
+                  </span>
+                </p>
 
-      <div className={'prose h-fit'}>
-        <Image src={loginImgSrc} alt={'ad'} className={`w-full`} />
-        <h1 className={'text-white text-center'}>
-          Bem vindo ao English Helper
-        </h1>
-        <p className={'text-white text-center'}>
-          Estamos aqui para ajudar você a alcançar uma comunicação mais
-          <span className={'text-yellow-400'}> precisa e impactante</span>,
-          elevando seu vocabulário e aperfeiçoando sua gramática.
-        </p>
-      </div>
-    </LoginSplitLayout>
+                <LoadingButton
+                  loading={loading}
+                  disabled={loading}
+                  className={`${styles.loginBtn} btn btn-primary mt-6`}
+                  type="submit"
+                >
+                  Entrar
+                </LoadingButton>
+              </div>
+            </form>
+          </>
+        )}
+      </Formik>
+    </div>
   );
 }
