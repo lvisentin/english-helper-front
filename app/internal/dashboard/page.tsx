913bbcd4807e500307880d1a0bb316494a13313f
lvisentin <lvise.batista@gmail.com>
2023-07-18 14:51:49 -0300

    feat: transition shit

--- app/internal/dashboard/page.tsx
+++ app/internal/dashboard/page.tsx
@@ -1,41 +1,47 @@
-function Dashboard() {
+'use client';
+import PageTransition from '@/shared/components/PageTransition/PageTransition';
+
+function Dashboard(props: any, ref: any) {
   return (
-    <main className="prose w-full max-w-full">
-      <div className="header">
-        <h2 className="mt-0">Bem vindo, lucas!</h2>
-        <div className="cards flex align-items-center gap-10">
-          <div className="card w-56 bg-base-100 shadow-lg">
-            <div className="card-body p-6">
-              <h2 className="card-title m-0">0</h2>
-              <p className="m-0">Cenários praticados</p>
+    <PageTransition ref={ref}>
+      <main className="prose w-full max-w-full">
+        <div className="header">
+          <h2 className="mt-0">Bem vindo, lucas!</h2>
+          <div className="cards flex align-items-center gap-10">
+            <div className="card w-56 bg-base-100 shadow-lg">
+              <div className="card-body p-6">
+                <h2 className="card-title m-0">0</h2>
+                <p className="m-0">Cenários praticados</p>
+              </div>
             </div>
-          </div>
-          <div className="card w-56 bg-base-100 shadow-lg">
-            <div className="card-body p-6">
-              <h2 className="card-title m-0">0</h2>
-              <p className="m-0">Palavras analisadas</p>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="content">
-        <h2 className="mt-5">Praticar cenários</h2>
-        <div className="scenarios">
-          <div className="card w-full bg-base-100 shadow-lg">
-            <div className="card-body p-6">
-              <h2 className="card-title m-0">Daily standup</h2>
-              <p className="m-0">
-                Ao se reunir para a standup, relate que teve um diálogo com
-                Daniel sobre a falta de recursos disponíveis para a
-                implementação de uma nova funcionalidade. Garanta à equipe que,
-                no momento, não há obstáculos que estejam impactando o andamento
-                do projeto relacionado a essa funcionalidade específica.
-              </p>
+            <div className="card w-56 bg-base-100 shadow-lg">
+              <div className="card-body p-6">
+                <h2 className="card-title m-0">0</h2>
+                <p className="m-0">Palavras analisadas</p>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </main>
+        <div className="content">
+          <h2 className="mt-5">Praticar cenários</h2>
+          <div className="scenarios">
+            <div className="card w-full bg-base-100 shadow-lg">
+              <div className="card-body p-6">
+                <h2 className="card-title m-0">Daily standup</h2>
+                <p className="m-0">
+                  Ao se reunir para a standup, relate que teve um diálogo com
+                  Daniel sobre a falta de recursos disponíveis para a
+                  implementação de uma nova funcionalidade. Garanta à equipe
+                  que, no momento, não há obstáculos que estejam impactando o
+                  andamento do projeto relacionado a essa funcionalidade
+                  específica.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+    </PageTransition>
   );
 }
 
--- app/internal/layout.tsx
+++ app/internal/layout.tsx
@@ -2,6 +2,7 @@ import Sidebar from '@/components/Sidebar/Sidebar';
 import Head from 'next/head';
 import React from 'react';
 import styles from './layout.module.scss';
+
 export interface IInternalLayout {
   children: React.ReactNode;
 }
@@ -14,6 +15,7 @@ const InternalLayout: React.FC<IInternalLayout> = ({ children }) => {
       </Head>
       <div className={`main flex flex-row h-full w-full`}>
         <Sidebar />
+
         <main
           className={`p-8 flex-grow bg-slate-100 overflow-auto ${styles.main}`}
         >
@@ -23,5 +25,4 @@ const InternalLayout: React.FC<IInternalLayout> = ({ children }) => {
     </>
   );
 };
-
 export default InternalLayout;
