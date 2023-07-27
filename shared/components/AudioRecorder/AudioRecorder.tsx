fce6db43178f2d87318bbaa66c4e778a63171bbd
lvisentin <lvise.batista@gmail.com>
2023-07-27 12:53:17 -0300

    fix: fixing audio issues

--- shared/components/AudioRecorder/AudioRecorder.tsx
+++ shared/components/AudioRecorder/AudioRecorder.tsx
@@ -5,7 +5,8 @@ import {
   faMicrophoneSlash,
 } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import { toast } from 'react-toastify';
 import LoadingButton from '../LoadingButton/LoadingButton';
 import styles from './AudioRecorder.module.scss';
 
@@ -24,7 +25,6 @@ function AudioRecorder({
   onStopRecording?: any;
   loading: boolean;
 }) {
-  const [permission, setPermission] = useState<boolean>(false);
   const mediaRecorder = useRef<any>(null);
   const [recordingStatus, setRecordingStatus] = useState<string>('inactive');
   const [stream, setStream] = useState<any>(null);
@@ -65,25 +65,23 @@ function AudioRecorder({
   }
 
   async function getMicrophonePermission() {
-    console.log('getMicrophonePermission');
     if ('MediaRecorder' in window) {
       try {
         const streamData = await navigator.mediaDevices.getUserMedia({
           audio: true,
           video: false,
         });
-        setPermission(true);
         setStream(streamData);
       } catch (err: any) {
-        alert(err.message);
+        toast.error('Por favor, autorize o uso do microfone');
       }
     } else {
-      alert('The MediaRecorder API is not supported in your browser.');
+      toast.error('The MediaRecorder API is not supported in your browser.');
     }
   }
 
   async function handleRecordingClick() {
-    if (!permission) {
+    if (!stream || !stream.active) {
       getMicrophonePermission();
     } else {
       if (recordingStatus === 'recording') {
@@ -93,6 +91,10 @@ function AudioRecorder({
       }
     }
   }
+
+  useEffect(() => {
+    getMicrophonePermission();
+  }, []);
 
   return (
     <>
@@ -104,13 +106,16 @@ function AudioRecorder({
             </p>
           ) : (
             <p className="prose-p text-center mb-4">
-              Clique no botão abaixo para iniciar a gravação...
+              {audio
+                ? 'Você pode ouvir seu áudio no player abaixo'
+                : 'Clique no botão abaixo para iniciar a gravação...'}
             </p>
           )}
         </div>
         <button
           type="button"
           className={`${styles.recordBtn} btn btn-primary rounded-full my-auto`}
+          disabled={audio}
           onClick={handleRecordingClick}
         >
           {recordingStatus === 'recording' ? (
